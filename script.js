92e6bfe62c897df397b1bfd8ed2eefcf225abbfc
Srijan <chaturchacharost@gmail.com>
2023-09-09 11:16:26 +0530

    added weater api request and output

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+//read
 const button = document.getElementById('cityBtn');
 const input = document.getElementById('cityName');
+//update
+const cityLoc = document.getElementById('location');
+const time = document.getElementById('locTime');
+const temp = document.getElementById('locTemp');
 
-button.addEventListener('click' , () =>{
-        //const city = input.value;
-        console.log(input.value);
+async function getData(city){
+    const promise = await fetch(`http://api.weatherapi.com/v1/current.json?key=8001e73922a34fe8aa545512230909&q=${city}&aqi=yes
+    `);
+    return await promise.json();
+}
+
+button.addEventListener('click' , async () =>{
+    let city = input.value;
+    const result = await getData(city)
+    cityLoc.innerText = `${result.location.name} , ${result.location.region} , ${result.location.country}`;
+    time.innerText = `${result.location.localtime}`;
+    temp.innerText = `${result.current.temp_c}`;
 });
