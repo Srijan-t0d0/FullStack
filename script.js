2392479716ef76b65546940054f97f123005570c
Srijan <chaturchacharost@gmail.com>
2023-09-09 10:49:39 +0530

    added js logic for userinput

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('cityBtn');
+const input = document.getElementById('cityName');
+
+button.addEventListener('click' , () =>{
+        //const city = input.value;
+        console.log(input.value);
+});
