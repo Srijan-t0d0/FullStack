4acc61dae9ea6e843d1c048223d6fb03a5432cda
Srijan <chaturchacharost@gmail.com>
2023-09-09 11:32:19 +0530

    added enter to submit

--- script.js
+++ script.js
@@ -10,12 +10,19 @@ async function getData(city){
     const promise = await fetch(`http://api.weatherapi.com/v1/current.json?key=8001e73922a34fe8aa545512230909&q=${city}&aqi=yes
     `);
     return await promise.json();
-}
+};
 
-button.addEventListener('click' , async () =>{
+async function fNr() {
     let city = input.value;
     const result = await getData(city)
-    cityLoc.innerText = `${result.location.name} , ${result.location.region} , ${result.location.country}`;
+    cityLoc.innerText = `${result.location.name} , ${result.location.region} - ${result.location.country}`;
     time.innerText = `${result.location.localtime}`;
     temp.innerText = `${result.current.temp_c}`;
+};
+
+button.addEventListener('click' , fNr );
+input.addEventListener('keypress' , (e) => {
+    if(e.key === "Enter") {
+        fNr()
+    }
 });
