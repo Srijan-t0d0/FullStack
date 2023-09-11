a56412e29d335b539fcee79c47c125359cbb4ecc
Srijan <chaturchacharost@gmail.com>
2023-09-12 00:16:21 +0530

    winner logic added

--- basics/tikTack/tikTack.js
+++ basics/tikTack/tikTack.js
@@ -1,13 +1,30 @@
 let currentPlayer = "X"
 let board = Array(9).fill(null)
+
+
+
+function checkWin(){
+    if(
+        (board[0] !== null && board[0] === board[1] && board[0] === board[2]) ||
+        (board[3] !== null && board[3] === board[4] && board[3] === board[5]) ||
+        (board[6] !== null && board[6] === board[7] && board[6] === board[8]) ||
+        (board[0] !== null && board[0] === board[3] && board[0] === board[6]) ||
+        (board[1] !== null && board[1] === board[4] && board[1] === board[7]) ||
+        (board[2] !== null && board[2] === board[5] && board[2] === board[8]) ||
+        (board[0] !== null && board[0] === board[4] && board[0] === board[8]) ||
+        (board[6] !== null && board[6] === board[4] && board[6] === board[2])
+        ){
+            console.log(currentPlayer)
+        }
+    }
+    
 function handleClick(el){
     const id = Number(el.id);
     if(board[id] === null){
         el.innerText = currentPlayer;
         board[id] = currentPlayer;
+        checkWin();
         currentPlayer = currentPlayer === 'X' ?  '0' : 'X';
         console.log(board);
     }
-    
-    
 }
